40df36190f09b216938d9e414dff271ba993b4bd
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-10-01 11:48:53 +0530

    view product added

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Navbar from "../Home/Navbar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import heart from "../imgs/heart.png";
 import heart_red from "../imgs/red-heart.png";
 import rating from "../imgs/rating.png";
+import { NavLink } from "react-router-dom";
 
 function Wishlist() {
   const wishlist = useSelector((state) => state.wishlist.wishlist);
+  const dispatch = useDispatch();
+
+  // Function to toggle wishlist item
+  const toggleWishlist = (item) => {
+    dispatch({
+      type: "TOGGLE_WISHLIST_ITEM",
+      payload: item,
+    });
+  };
 
   return (
     <>
       <Navbar />
-      <div className="bg-gray-100 h-auto py-5">
+      <div className="bg-gray-100 h-auto pb-10 py-3">
         <h1 className="text-center font-bold text-3xl py-5">Wishlist</h1>
         <div className="w-[80%] h-auto mx-auto  flex justify-center">
           {wishlist.length > 0 ? (
@@ -28,6 +38,13 @@ function Wishlist() {
                         src={item.image}
                         alt={item.title}
                       />
+                      <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50 rounded-xl z-10">
+                        <NavLink to={`/product/${item.id}`} key={item.id}>
+                          <button className="bg-white text-black rounded-md p-4 font-bold text-[15px]">
+                            View product
+                          </button>
+                        </NavLink>
+                      </div>
                       <div className="absolute top-3 right-3 z-20">
                         <img
                           onClick={() => toggleWishlist(item)}
@@ -60,14 +77,14 @@ function Wishlist() {
                         <img className="w-4" src={rating} alt="" />
                       </div>
                       <div>
-                        <p className="text-sm font-bold">
-                          {"5 " + "(" + item.rating.count + " reviews)"}
-                        </p>
+                        <p className="text-sm font-bold mx-2">5</p>
                       </div>
                     </div>
                     <div className="flex mx-2 gap-2 pb-3">
                       <p className="text-black font-bold">${item.price}</p>
-                      <p className="line-through">${Math.round(item.price * 1.66)}</p>
+                      <p className="line-through">
+                        ${Math.round(item.price * 1.66)}
+                      </p>
                       <span className="text-red-600 font-bold">(60%)</span>
                     </div>
                   </div>
